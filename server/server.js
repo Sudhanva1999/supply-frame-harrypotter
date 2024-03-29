5552227037bcd19b21f5a5ba770efed7ee33238e
Sudhanva1999 <49947142+Sudhanva1999@users.noreply.github.com>
2024-03-28 21:35:30 -0400

    Adding cache , added more details to details page, added recommendations for pokemons.

--- server/server.js
+++ server/server.js
@@ -8,68 +8,92 @@ const PORT = process.env.PORT || 3000;
 const HOST = '0.0.0.0';
 const cache = new NodeCache({ stdTTL: 3600 });
 
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + '/public'));
 
+function cacheMiddleware(req, res, next) {
+    const cacheKey = req.originalUrl;
+    const cachedData = cache.get(cacheKey);
+    if (cachedData) {
+        res.send(cachedData);
+    } else {
+        res.sendResponse = res.send;
+        res.send = (body) => {
+            cache.set(cacheKey, body);
+            res.sendResponse(body);
+        };
+        next();
+    }
+}
 
-app.get('/', async (req, res) => {
+app.get('/', cacheMiddleware, async (req, res) => {
     try {
         res.render('index');
     } catch (error) {
-      console.error('Error fetching Pokemon data:', error);
-      res.status(500).send('Internal Server Error');
+        console.error('Error fetching Pokemon data:', error);
+        res.status(500).send('Internal Server Error');
     }
-  });
-
-
-app.get('/list', async (req, res) => {
-  try {
-    let pokemonList = [];
-
-    const page = parseInt(req.query.page) || 1;
-    const pageSize = 20;
-    const startIdx = (page - 1) * pageSize + 1;
-    const endIdx = startIdx + pageSize - 1;
-
-   
-    const cacheKey = `${startIdx}-${endIdx}`;
-    const cachedData = cache.get(cacheKey);
-    if (cachedData) {
-      pokemonList = cachedData;
-    } else {
-      
-      for (let i = startIdx; i <= endIdx; i++) {
-        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}/`);
-        const pokemonDetails = response.data;
-        pokemonList.push(pokemonDetails);
-      }
-      
-      cache.set(cacheKey, pokemonList);
-    }
-    res.render('list', { pokemonList });
-
-  } catch (error) {
-    console.error('Error fetching Pokemon data:', error);
-    res.status(500).send('Internal Server Error');
-  }
 });
 
+app.get('/list', cacheMiddleware, async (req, res) => {
+    try {
+        let pokemonList = [];
+        const page = parseInt(req.query.page) || 1;
+        const pageSize = 20;
+        const startIdx = (page - 1) * pageSize + 1;
+        const endIdx = startIdx + pageSize - 1;
+        const cacheKey = `${startIdx}-${endIdx}`;
 
-app.get('/pokemon', async (req, res) => {
-  try {
-    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${req.query.pokemonId}/`);
-    const pokemon = response.data;
-    res.render('pokemon', { pokemon });
-
-  } catch (error) {
-    console.error('Error fetching Pokemon data:', error);
-    res.status(500).send('Internal Server Error');
-  }
+        const cachedData = cache.get(cacheKey);
+        if (cachedData) {
+            pokemonList = cachedData;
+        } else {
+            for (let i = startIdx; i <= endIdx; i++) {
+                const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+                const pokemonDetails = response.data;
+                pokemonDetails.pokemonMainImg = pokemonDetails.sprites.other.showdown.front_default || pokemonDetails.sprites.other.home.front_default;
+                pokemonList.push(pokemonDetails);
+            }
+            cache.set(cacheKey, pokemonList);
+        }
+        res.render('list', { pokemonList });
+    } catch (error) {
+        console.error('Error fetching Pokemon data:', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+app.get('/pokemon', cacheMiddleware, async (req, res) => {
+    try {
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${req.query.pokemonId}/`);
+        const pokemon = response.data;
+        pokemon.pokemonMainImg = pokemon.sprites.other.showdown.front_default || pokemon.sprites.other.home.front_default;
+
+        const typeResponse = await axios.get(pokemon.types[0].type.url);
+        const samePokeLimit = getRandomInt(180);
+        const allSamePokemons = typeResponse.data.pokemon.slice(samePokeLimit, samePokeLimit + 8);
+        const pokemonDataArray = [];
+
+        for (const samePokemon of allSamePokemons) {
+            if (samePokemon.pokemon.name !== pokemon.name) {
+                const pokemonResponse = await axios.get(samePokemon.pokemon.url);
+                const tempPokemon = pokemonResponse.data;
+                tempPokemon.pokemonMainImg = tempPokemon.sprites.other.showdown.front_default || tempPokemon.sprites.other.home.front_default;
+                pokemonDataArray.push(pokemonResponse.data);
+            }
+        }
+        res.render('pokemon', { pokemon, pokemonDataArray });
+    } catch (error) {
+        console.error('Error fetching Pokemon data:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
 app.listen(PORT, HOST, () => {
-  console.log(`Server is running on http://${HOST}:${PORT}`);
+    console.log(`Server is running on http://${HOST}:${PORT}`);
 });
